f1d9c2064de5a4a170c30e0ffcad02d5baa4c948
luiscotoo <luiscoto001@gmail.com>
2021-08-09 15:03:44 -0600

    Modificacion

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import './App.css';
 
 
 function App() {
-  const [unidad, setUnidad] = useState(0);
-  const [decena, setDecena] = useState(0);
-  const [centena, setCentena] = useState(0);
-  const [umil, setUmil] = useState(0);
-  const [dmil, setDmil] = useState(0);
-  const [cmil, setCmil] = useState(0);
+  const [dolar, setDolar] = useState(0);
+  const [euro, setEuro] = useState(0);
+  const [bitcoin, setBitcoin] = useState(0);
+  
 
   const [tipo, setTipo] = useState(1);
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    setUnidad((total * tipo) / 1);
-    setDecena((total * tipo) / 10);
-    setCentena((total * tipo) / 100);
-    setUmil((total * tipo) / 1000);
-    setDmil((total * tipo) / 10000);
-    setCmil((total * tipo) / 100000);
+    setDolar((total * tipo)/1);
+    setEuro((total * tipo)/1.17);
+    setBitcoin((total * tipo)/46400.30);
   }, [total, tipo]);
 
 
@@ -32,26 +27,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Convertidor de unidades</h1>
+      <h1>Convertidor de divisas</h1>
 
-      <p>Unidad: {unidad}</p>
-      <p>Decena: {decena}</p>
-      <p>Centena: {centena}</p>
-      <p>Unidad Mil: {umil}</p>
-      <p>Decena Mil: {dmil}</p>
-      <p>Centena Mil: {cmil}</p>
+      <p>Dolar: {dolar}</p>
+      <p>Euro: {euro}</p>
+      <p>Bitcoin: {bitcoin}</p>
 
       <hr />
 
       <h3>{total}</h3>
 
       <select onChange={event => setTipo(event.target.value)} value={tipo}>
-        <option value={1}>Unidad</option>
-        <option value={10}>Decena</option>
-        <option value={100}>Centena</option>
-        <option value={1000}>Unidad Mil</option>
-        <option value={10000}>Decena Mil</option>
-        <option value={100000}>Centena Mil</option>
+        <option value={1}>Dolar</option>
+        <option value={1.17}>Euro</option>
+        <option value={46400.30}>Bitcoin</option>
       </select>
       <input onChange={handleTotalChange} value={total} />
     </div>
